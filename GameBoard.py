e59bd5fe611692b9d8e2c1b034df8327882234e7
mahira-manico <mahira.manico@laplateforme.io>
2026-03-25 15:16:30 +0100

    feat: gameboard class done

--- GameBoard.py
+++ GameBoard.py
@@ -21,7 +21,7 @@ class GameBoard:
         while mine_placed<mines_total:
             x_aleatory=random.randint(0,self.column-1)
             y_aleatory=random.randint(0,self.line-1)
-            target_case=self.grid_board[y_aleatory, x_aleatory]
+            target_case=self.grid_board[y_aleatory][x_aleatory]
             if target_case.is_mine:
                 target_case.place_mine()
                 mine_placed+=1
@@ -31,7 +31,7 @@ class GameBoard:
         if x<0 or x>=self.column or y<0 or y>=self.line:
             return
         
-        target_case=self.grid_board[y,x]
+        target_case=self.grid_board[y][x]
         if target_case.is_mine or target_case.is_flag or target_case.is_revealed or target_case.is_point:
             return
         
@@ -43,4 +43,24 @@ class GameBoard:
         self.flood_fill(x+1,y)
         self.flood_fill(x,y-1)
         self.flood_fill(x,y+1)
+    
+    def generate_number(self):
+        for y in range(self.line):
+         for x in range(self.column):
+           actual_case=self.grid_board[y][x]
+           if actual_case.is_mine==False:
+            mines_around=0
+            for dy in [-1,0,1]:
+                for dx in [-1,0,1]:
+                    x_around=x+dx
+                    y_around=y+dy
+                    if 0<=x_around<self.column and 0<=y_around<self.line:
+                        neighbor_case=self.grid_board[y_around][x_around]
+                        if neighbor_case.is_mine==True:
+                            mines_around+=1
+                            actual_case.mine_around(mines_around)
+                    
+
+
+
 
